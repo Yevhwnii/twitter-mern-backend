410f186235aa118d461ae62f41fb04caa56eac8e
Yevhwnii <mirandoos3223@gmail.com>
2020-10-24 16:42:16 +0200

    user routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express';
+require('dotenv').config();
+
+import './core/db';
+import { UserCtrl } from './controllers/UserController';
+import { signupValidation } from './validators/signup';
 
 const app = express();
 
-app.get('/hello', (_, res: express.Response) => {
-  res.send('World');
-});
+app.use(express.json());
 
-app.listen(8888, (): void => {
+app.get('/users', UserCtrl.index);
+app.post('/users', signupValidation, UserCtrl.create);
+app.get('/users/verify', signupValidation, UserCtrl.verify);
+// app.patch('/users', UserCtrl.update)
+// app.delete('/users', UserCtrl.delete)
+
+app.listen(process.env.PORT || 8888, (): void => {
   console.log('Server is running!');
 });
