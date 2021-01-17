2ebe7ef579ef04d880fedaeffb34fcbd4dca7f0a
Yevhwnii <mirandoos3223@gmail.com>
2021-01-17 17:55:54 +0100

    tweet model initial version

---
+++ src/models/Tweet.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface ITweet extends Document {
+  text: string;
+  user: string;
+}
+
+export interface ITweet_raw {
+  _id?: string;
+  text: string;
+  user: string;
+}
+
+const TweetSchema: Schema = new Schema({
+  text: { type: String, required: true, maxlength: 280 },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+export const TweetModel = mongoose.model<ITweet>('Tweet', TweetSchema);
