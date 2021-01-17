5b34ae836f27fb1a5c3568a0829f85a91abc74a7
Yevhwnii <mirandoos3223@gmail.com>
2021-01-17 17:56:09 +0100

    tweet controller initial CRUD

---
+++ src/controllers/TweetsController.ts
@@ -0,0 +1,107 @@
+require('dotenv').config();
+import express from 'express';
+import { validationResult } from 'express-validator';
+
+import { ITweet, ITweet_raw, TweetModel } from '../models/Tweet';
+import { IUser } from '../models/User';
+import { isValidObjectId } from '../utils/isValidObjectId';
+
+class TweetsController {
+  async index(_: any, res: express.Response): Promise<void> {
+    try {
+      const tweets = await TweetModel.find({}).exec();
+      res.json({
+        status: 'success',
+        data: tweets,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  }
+
+  async show(req: any, res: express.Response): Promise<void> {
+    try {
+      const tweetId = req.params.id;
+      if (!isValidObjectId(tweetId)) {
+        throw new Error('Id is not valid (must be objectId)');
+      }
+      const tweet = await TweetModel.findById(tweetId).exec();
+      if (tweet) {
+        res.json({
+          status: 'success',
+          data: tweet,
+        });
+      } else {
+        throw new Error('Tweet does not exist');
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  async create(req: express.Request, res: express.Response): Promise<void> {
+    try {
+      const user = req.user as IUser;
+      if (user) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          res.status(400).json({ status: 'error', errors: errors.array() });
+          return;
+        }
+
+        const tweetDetails: ITweet_raw = {
+          text: req.body.text,
+          user: user._id,
+        };
+
+        const tweet = await TweetModel.create(tweetDetails);
+
+        res.json({
+          status: 'success',
+          data: tweet,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  async delete(req: express.Request, res: express.Response): Promise<void> {
+    const user = req.user as IUser;
+
+    try {
+      if (user) {
+        const tweetId = req.params.id;
+
+        if (!isValidObjectId(tweetId)) {
+          throw new Error('Id is not valid (must be objectId)');
+        }
+
+        const tweet = await TweetModel.findById(tweetId);
+
+        if (tweet && tweet.user.toString() === user._id.toString()) {
+          tweet.remove();
+          res.send();
+        } else {
+          res.status(404).send();
+        }
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+}
+
+export const TweetsCtrl = new TweetsController();
